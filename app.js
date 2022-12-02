e7bccc879050c1cf5ecb14ce3f0885b9887a9425
aBgAmeuR <a.josset@yahoo.com>
2022-12-02 03:59:41 +0100

    add cookie
    add cookie with JS

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 // header words
 const positif = document.querySelectorAll("#positif div");
 const negatif = document.querySelectorAll("#negatif div");
-positif.forEach(e => {
-  const top = Math.round(Math.random() * (window.screen.height * 0.9 - window.screen.height * 1));
-  const left = Math.round(Math.random() * (window.screen.width * 0.9 - window.screen.width * 1));
+positif.forEach((e) => {
+  const top = Math.round(
+    Math.random() * (window.screen.height * 0.9 - window.screen.height * 1)
+  );
+  const left = Math.round(
+    Math.random() * (window.screen.width * 0.9 - window.screen.width * 1)
+  );
   e.style.top = `${top}px`;
   e.style.left = `${left}px`;
 });
-negatif.forEach(e => {
-  const top = Math.round(Math.random() * (window.screen.height * 0.8 - window.screen.height * 0.2)) + 50;
-  const left = Math.round(Math.random() * (window.screen.width * 0.8 - window.screen.width * 0.2)) + 50;
+negatif.forEach((e) => {
+  const top =
+    Math.round(
+      Math.random() * (window.screen.height * 0.8 - window.screen.height * 0.2)
+    ) + 50;
+  const left =
+    Math.round(
+      Math.random() * (window.screen.width * 0.8 - window.screen.width * 0.2)
+    ) + 50;
   e.style.top = `${top}px`;
   e.style.left = `${left}px`;
 });
+
+function loadcookies() {
+  if (!document.cookie) {
+    uniqueId = Math.random().toString(36).substr(2, 9) + Math.random().toString(36).substr(2, 9) + Math.random().toString(36).substr(2, 9) + Math.random().toString(36).substr(2, 9);
+    const cookie = `cookie=${uniqueId}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+    document.cookie = cookie;
+  }
+  console.log(document.cookie);
+}
+loadcookies();
--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const mongoose = require('mongoose');
-const {DATABASE_URI} = require("./config.json");
+const mongoose = require("mongoose");
+const { DATABASE_URI } = require("./config.json");
 const news = require("./models/news.model");
+
 // database connection
 //Le proxi du serveur de l'esia peut le bloquer, alors pour l'instant on laisse comme ça.
-mongoose.connect(DATABASE_URI, {
+mongoose
+  .connect(DATABASE_URI, {
     autoIndex: false, // Don't build indexes
     maxPoolSize: 10, // Maintain up to 10 socket connections
     serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
     socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    family: 4 // Use IPv4, skip trying IPv6
-}).then(() => { console.log('Successfully connected to the database !') })
-.catch(err => { console.log(err) });
-
+    family: 4, // Use IPv4, skip trying IPv6
+  })
+  .then(() => {
+    console.log("Successfully connected to the database !");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 tracerPos = ["zero", "one", "two", "three"];
 let nbTracer = 0;
@@ -20,21 +26,21 @@ function nextStep() {
   document.getElementById("tracer").classList.add(tracerPos[++nbTracer]);
 }
 
-
-
-
-
-
 // actus
-const actus = async() => {
-    const newsModel = await news.findOne({ id:1});
+const actus = async () => {
+  const newsModel = await news.findOne({ id: 1 });
 
-    if(!newsModel) {
-        const newsM = new news({id: 1, date: '23 février 2022', title: 'INFO SERVICE GUYANE', desc: "Sida Info Service ouvre ses services d'écoute en Guyane. La ligne est gratuite et accessible chaque lundi, mercredi et vendredi de 17 h à 23 h. Ces services sont disponibles en français, créole, créole haïtien, portugais et espagnol. Au cours du second semestre 2022, SIS Association élargira les créneaux horaires d’écoute et proposera de nouvelles langues."})
-        await newsM.save();
-    } else {
-        console.log(newsModel.date)
-    }
-}
+  if (!newsModel) {
+    const newsM = new news({
+      id: 1,
+      date: "23 février 2022",
+      title: "INFO SERVICE GUYANE",
+      desc: "Sida Info Service ouvre ses services d'écoute en Guyane. La ligne est gratuite et accessible chaque lundi, mercredi et vendredi de 17 h à 23 h. Ces services sont disponibles en français, créole, créole haïtien, portugais et espagnol. Au cours du second semestre 2022, SIS Association élargira les créneaux horaires d’écoute et proposera de nouvelles langues.",
+    });
+    await newsM.save();
+  } else {
+    console.log(newsModel.date);
+  }
+};
 actus();
 
